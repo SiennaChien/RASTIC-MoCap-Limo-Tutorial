939bdd5c6c6d9c27e010a4a7c1833baa385192d5
SiennaChien <sienna.chien@gmail.com>
2024-07-12 14:45:50 -0400

    Update limo_cmd_listener.py

--- Tutorial 1/limo_cmd_listener.py
+++ Tutorial 1/limo_cmd_listener.py
@@ -4,7 +4,6 @@ import rospy
 from ackermann_msgs.msg import AckermannDrive
 import time
 import numpy as np
-#import pickle
 
 class listener:
     def __init__(self):
@@ -15,7 +14,7 @@ class listener:
 
     def listener(self):
         rospy.init_node("vs_listener", anonymous=True)
-        rospy.Subscriber("vel_steer_limo770", AckermannDrive, self.callback)
+        rospy.Subscriber("vel_steer_limo780", AckermannDrive, self.callback)
 
 
 if __name__ == '__main__':
@@ -25,16 +24,15 @@ if __name__ == '__main__':
     limo.SetMotionCommand(linear_vel=0, steering_angle=0)
     time.sleep(1)
     steering_angle = np.zeros(250)
-    #imu_yaw =  np.zeros(250)
     iter = 0 
     previous_steering = -6000 #arbitrary number
     while True:
         listener_ins.listener()
-        if previous_steering == listener_ins.data.steering_angle:
-            print("It is possible motion capture stopped updating, please double check and rerun")
-            limo.SetMotionCommand(linear_vel=0, steering_angle=0)
-            break
-        elif listener_ins.data is not None:
+        if listener_ins.data is not None:
+            if previous_steering == listener_ins.data.steering_angle:
+                print("it is possible the mocap stopped updating, double check and run again")
+                limo.SetMotionCommand(linear_vel=0, steering_angle=0)
+                break
             listener_ins.data.speed
             limo.SetMotionCommand(linear_vel=listener_ins.data.speed, steering_angle=listener_ins.data.steering_angle)
             print("Velocity: ", listener_ins.data.speed, "Steering: ", listener_ins.data.steering_angle)
@@ -45,10 +43,7 @@ if __name__ == '__main__':
             time.sleep(0.1)
         else:
             print("[WARNING] Not receiving ROS messages")
+            time.sleep(1)
+            limo.SetMotionCommand(linear_vel=0, steering_angle=0)
             if iter>10:
                 break
-    # outputFile = 'steering.pickle'
-    # data = {'imu_yaw':imu_yaw, 'steer_limo':steering_angle}
-    # fw = open(outputFile, 'wb')
-    # pickle.dump(data, fw)
-    # fw.close()
